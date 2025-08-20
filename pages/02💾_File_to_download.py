7142392e0f78d4272d5bdfb0ade3b8d53d0152a1
MK <99416359+MK316@users.noreply.github.com>
2025-08-21 05:20:45 +0900

    Update 02💾_File_to_download.py

--- pages/02💾_File_to_download.py
+++ pages/02💾_File_to_download.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 url = "https://github.com/jihyeon0531/WordApp/blob/main/data/wordlist-0821.pdf"
 
-st.markdown("1. [Word list file](https://github.com/jihyeon0531/WordApp/raw/main/data/wordlist-0821.pdf): 단어장 다운 받기 (pdf 파일; 총 6페잊)")
+st.markdown("1. [Word list file](https://github.com/jihyeon0531/WordApp/raw/main/data/wordlist-0821.pdf): 단어장 다운 받기 (pdf 파일; 총 6페이지)")
