eda2f18ad9768382ca24bf3bf01cd0ebde10f576
MK <99416359+MK316@users.noreply.github.com>
2025-08-20 21:13:49 +0900

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,1 +1,16 @@
 import streamlit as st
+
+st.markdown("### Welcome to Ms.Choi's English Classroo")
+st.caption("Since Aug 20, 2025")
+
+# Image links
+main_image_url = "https://github.com/jihyeon0531/WordApp/raw/main/images/welcome3.png"
+qr_image_url = "https://github.com/jihyeon0531/WordApp/raw/main/images/wordapp1.png"
+
+# Use columns to center images
+col1, col2, col3 = st.columns([1, 2, 1])  # middle column bigger
+
+with col1:
+    st.image(main_image_url, width=500, caption="Welcome Image")  # Expand button appears
+with col3:
+    st.image(qr_image_url, width=50, caption="QR")  # Expand button appears
