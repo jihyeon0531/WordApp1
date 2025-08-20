b73338e130eb83e9cf8572c7c291e6525e1c3b07
MK <99416359+MK316@users.noreply.github.com>
2025-08-21 04:55:09 +0900

    Update 02💾_File_to_download.py

--- pages/02💾_File_to_download.py
+++ pages/02💾_File_to_download.py
@@ -1,2 +1,5 @@
 import streamlit as st
 
+url = "https://github.com/jihyeon0531/WordApp/blob/main/data/wordlist-0821.pdf"
+
+st.markdown("1. [Word list file](https://github.com/jihyeon0531/WordApp/blob/main/data/wordlist-0821.pdf): 단어장 다운 받기 (pdf 파일; 총 6페잊)")
